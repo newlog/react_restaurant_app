2621d35af8c655147283c6caa7784894d234089c
albert.lopez@attackiq.com <albert.lopez@attackiq.com>
2020-05-17 18:04:32 +0200

    first commit after minimal component

---
+++ 2/confusion/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent'
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+    </div>
+  );
+}
+
+export default App;
