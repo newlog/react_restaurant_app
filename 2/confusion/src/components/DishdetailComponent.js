8b3c92f62945c60d27822e66c6405c6aea240950
newlog <newlog@overflowedminds.net>
2020-05-18 14:05:49 +0200

    Render dish and comments side by side

--- 2/confusion/src/components/DishdetailComponent.js
+++ 2/confusion/src/components/DishdetailComponent.js
@@ -6,14 +6,20 @@ class Dishdetail extends Component {
     renderDish() {
         const dish = this.props.selectedDish;
         if (dish != null) {
+            const comments = this.renderComments()
             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="row">
+                <div key={dish.id} className="col-12 col-sm-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                {comments}
+                </div>
             );
         } else {
             return (
@@ -22,11 +28,40 @@ class Dishdetail extends Component {
         }
     }
 
+    renderComments() {
+        const dish = this.props.selectedDish;
+        const commentDetail = this.getCommentDetails(dish)
+        return (
+            <div className="col-12 col-md-5 m-1">
+                {commentDetail}
+            </div>
+        );
+    }
+
+    getCommentDetails(dish) {
+        return dish.comments.map(comment =>
+            <div key={comment.id}>
+                <div className="row">
+                    {comment.author} - {comment.date}
+                </div>
+                <div className="row">
+                    Rating: {comment.rating}
+                </div>
+                <div className="row">
+                    <b>Comment:</b>
+                </div>
+                <div className="row">
+                    <p>
+                        {comment.comment}
+                    </p>
+                </div>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div className="row">
-                {this.renderDish()}
-            </div>
+            this.renderDish()
         );
     }
 }
