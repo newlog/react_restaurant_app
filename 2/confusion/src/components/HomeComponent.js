b2b094c01af833c8bccecbd4420a3a0e7ff8f375
newlog <newlog@overflowedminds.net>
2020-05-24 13:28:38 +0200

    SPA: decomposing data. Adding home page data.

--- 2/confusion/src/components/HomeComponent.js
+++ 2/confusion/src/components/HomeComponent.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from 'reactstrap';
 
-function Home() {
+function RenderCard({ item }) {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {
+          // adding JS code inside the HTML code
+          item.designation ? (
+            <CardSubtitle>{item.designation}</CardSubtitle>
+          ) : null
+        }
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+function Home(props) {
+  const { dish, promotion, leader } = props;
   return (
     <div className="container">
-      <h4>Home</h4>
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={dish} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={leader} />
+        </div>
+      </div>
     </div>
   );
 }
--- 2/confusion/src/components/MainComponent.js
+++ 2/confusion/src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
+// import DishDetail from './DishdetailComponent';
 import DISHES from '../shared/dishes';
+// import COMMENTS from '../shared/comments';
+import LEADERS from '../shared/leaders';
+import PROMOTIONS from '../shared/promotions';
 
 class Main extends Component {
   constructor(props) {
@@ -13,16 +17,28 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      // comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     // this line is to avoid eslint "Must use destructuring state assignment"
     // https://stackoverflow.com/questions/52638426/eslint-must-use-destructuring-state-assignment
-    const { dishes } = this.state;
+    const { dishes, promotions, leaders } = this.state;
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={dishes.filter((dish) => dish.featured === true)[0]}
+          promotion={
+            promotions.filter((promotion) => promotion.featured === true)[0]
+          }
+          leader={leaders.filter((leader) => leader.featured === true)[0]}
+        />
+      );
     };
+
     return (
       <div>
         <Header />
@@ -33,6 +49,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
